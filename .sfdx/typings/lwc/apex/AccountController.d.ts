552d06b9ad557251ccf846b083be3aa253270424
Krishna Reddy <krishnareddy@Krishnas-MacBook-Pro.local>
2025-03-06 17:00:27 -0600

    initial commit

---
+++ .sfdx/typings/lwc/apex/AccountController.d.ts
@@ -0,0 +1,6 @@
+declare module "@salesforce/apex/AccountController.getAllActiveAccounts" {
+  export default function getAllActiveAccounts(): Promise<any>;
+}
+declare module "@salesforce/apex/AccountController.updateAccounts" {
+  export default function updateAccounts(param: {data: any}): Promise<any>;
+}
